04e1512c5af6d9028aff643ad405f55f404ff1c1
bpv834 <bpv8342Gmail.com>
2025-05-23 16:55:29 +0900

    N과 M (4) 중복조합

--- boj15652/src/Boj15652.java
+++ boj15652/src/Boj15652.java
@@ -1,2 +1,41 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Boj15652 {
+    static int n;
+    static int m;
+    static int [] arr;
+    static BufferedReader br;
+    static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        StringTokenizer tk =new StringTokenizer(str);
+
+        n = Integer.parseInt(tk.nextToken());
+        m = Integer.parseInt(tk.nextToken());
+        arr=new int[m];
+        dfs(0,1);
+
+        bw.flush();  // 꼭 flush 해줘야 출력됨
+        bw.close();
+
+    }
+
+    static void dfs(int L,int x) throws IOException {
+        if(L==m){
+            for (int i = 0; i < L; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.newLine();  // 줄바꿈
+
+        }else{
+            for (int i = x; i <= n; i++) {
+                arr[L]=i;
+                dfs(L+1,i);
+            }
+        }
+    }
 }
