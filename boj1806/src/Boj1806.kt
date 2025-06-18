082c775686f4950c68354f0dc4e4d1e6c6d71b16
bpv834 <yungjoon2@naver.com>
2025-06-19 00:41:45 +0900

    부분합

---
+++ boj1806/src/Boj1806.kt
@@ -0,0 +1,24 @@
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, s) = br.readLine().split(" ").map { it.toInt() }
+    val list = br.readLine().split(" ").map { it.toInt() }
+    var ans = Int.MAX_VALUE
+    var sum = 0
+    var lt = 0
+
+    for (rt in 0 until n) {
+        sum += list[rt]
+
+        while (sum >= s) {
+            val cnt = rt - lt + 1
+            ans=Math.min(ans, cnt)
+            sum -= list[lt++]
+        }
+
+    }
+    if (ans == Int.MAX_VALUE) println(0) else println(ans)
+
+}
